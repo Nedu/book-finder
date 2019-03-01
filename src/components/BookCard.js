a16a282d1f09067d670110fc48547de8bb45b304
Nedu Robert <nedumrobert@gmail.com>
2019-03-01 13:25:12 +0100

    Feat: Implemented fetching books from google API and displaying them

--- src/components/BookCard.js
+++ src/components/BookCard.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import { Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle, Button } from 'reactstrap';
 
-const BookCard = () => (
-    <div>
-        BookCard component
-    </div>
-)
+const BookCard = (props) => {
+    console.log(props);
+    // eslint-disable-next-line react/prop-types
+    const { image, authors, title, publisher, previewLink } = props;
+    let bookCover;
+    // const count = 100;
+    if (!Object.prototype.hasOwnProperty.call(image, 'thumbnail')) {
+        bookCover = image;
+    } else {
+        bookCover = image.thumbnail
+    }
+    return <Card style={{ margin: '1em', width: '30%' }}>
+            <CardImg top width="100%" height="150px" style={{ objectFit: 'cover' }} src={bookCover} alt="Book cover image" />
+            <CardBody>
+                <CardTitle>Title: {title}</CardTitle>
+                <CardSubtitle>Author: {authors ? (authors.map(author => author)) : 'Authors not found'}</CardSubtitle>
+                <CardText>Publisher: {publisher || 'Publisher not found'}</CardText>
+                <Button><a href={previewLink}>More Info</a></Button>
+            </CardBody>
+    </Card>
+}
 
 export default BookCard;
